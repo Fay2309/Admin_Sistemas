dca8896b31630eb79ff1b9255d43b91f672c4e6b
Fay2309 <gaelvalenzuela2309@gmail.com>
2025-02-06 14:23:21 -0700

    Update Variables_TiposDatos.ps1

--- Variables_TiposDatos.ps1
+++ Variables_TiposDatos.ps1
@@ -1,1 +1,31 @@
 $variable1 = "Hola"
+
+$variable2 = " ¿que tal?"
+
+$VARiable3 = 100 
+
+${VAR iable4} = 200
+
+New-Variable -Name variable5 -Value 300 
+$variable5
+
+$variable1
+$variable2
+$VARiable3
+${VAR iable4}
+
+$variable1 + $variable2
+$VARiable3 + ${VAR iable4}
+$VARiable3 - ${VAR iable4}
+
+$$
+$^
+$?
+$Error
+
+$ConfirmPreference
+$DebugPreference
+$ErrorActionPreference
+$WarningPreference
+
+$variable1.GetType()
